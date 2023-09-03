4d1028a616deccad6a702b2a279a0dadbfdc7d54
muhammadalinoor-1982 <noor.web.journey@gmail.com>
2023-09-03 10:36:21 +0600

    03/09/2023 matplotlib start study

---
+++ matplotlib study/matplotlib.py
@@ -0,0 +1,14 @@
+# ****************************  Matplotlib Study **************************
+
+# Link: https://www.youtube.com/playlist?list=PLjVLYmrlmjGcC0B_FP3bkJ-JIPkV5GuZR
+
+# *********** Plot: linear, scatter, bar, stem, step, hist, box, pie, fill between/area plot  ***********
+
+# Install Jupyter Note Book: Go to Python Installation Folder then Go to Script Folder 
+# Then Type 'cmd' in the Address bar. Then Type "pip install jupyter" in command promt
+# Open Jupyter Notebook: Type "jupyter notebook" in the same command promt
+# Open a file in the jupyter notebook: Go to New and Select Notebook then Select python3 (Kernel)
+# Then Type "pip install matplotlib" in command promt
+# Use matplotlib in 2 ways: 'from matplotlib import pyplot as plt' OR 'import matplotlib.pyplot as plt' 
+
+
