f872cee44c2f2581f336ef11169718466dfdf46f
muhammadalinoor-1982 <noor.web.journey@gmail.com>
2023-09-20 09:26:53 +0600

    Bar and Histogram Plot

--- seaborn study/seaborn.py
+++ seaborn study/seaborn.py
@@ -35,3 +35,41 @@ plt.grid()
 plt.title('Penguins', fontsize=15)
 
 plt.show()
+
+# Histogram Plot with it's perameters with GitHub Dataset
+sns.displot(
+            df['bill_length_mm'], 
+            bins=[34,36,38,40,42,44,46], 
+            kde=True, 
+            rug=True,
+            color='brown',
+           ) # log_scale=True
+plt.show()
+
+# Bar Plot with GitHub Dataset
+sns.barplot(x=df.island, y=df.bill_length_mm)
+plt.show()
+
+# Bar Plot with it's perameters with GitHub Dataset
+sns.set(style='darkgrid')
+sns.barplot(
+            x='island', 
+            y='bill_length_mm', 
+            data=df, 
+            hue='sex',
+            order=['Dream', 'Torgersen', 'Biscoe'],
+            hue_order=['Female', 'Male'],
+            #ci=100
+            errorbar=('ci', 100),
+            n_boot=2,
+            orient='v',
+            #color='r' 
+            palette='icefire',
+            saturation=50,
+            errcolor='r',
+            errwidth=3,
+            capsize=0.2,
+            dodge=True,
+            alpha=1
+           )
+plt.show()
